089f70aa1ff9929320a794fb35d4bba9848cfd8d
sword4869 <1084662708@qq.com>
2024-08-30 14:21:01 +0800

    优化 cli

--- README.md
+++ README.md
@@ -1,100 +1,87 @@
-[README_ZH.md](https://github.com/sword4869/face_parsing/blob/master/README.md) | [README_EN.md](https://github.com/sword4869/face_parsing/blob/master/README_EN.md) 
-
 # 项目描述 
 
-该仓库用于生成人脸图像的语义分割。
+基于 [face-parsing.PyTorch](https://github.com/zllrunning/face-parsing.PyTorch), 该仓库用于生成人脸图像的语义分割。
 
 # 安装
 
 下载 https://drive.google.com/open?id=154JgKpzCPW82qINcVieuPH3fZ2e0P812 的权重文件 79999_iter.pth
-```python
-# pip install -e git+https://github.com/sword4869/face_parsing.git#egg=face_parsing
-pip install face_parsing
+
+```bash
+pip install git+https://github.com/sword4869/face_parsing.git#egg=face_parsing
 ```
 
 # 输入和输出
 ```
 ├── pretrain
 │   └── 79999_iter.pth      # ckpt
-├── test_img                # 输入
-│   ├── 00000.jpg
-│   └── 116_ori.png
-└── test_res                # 输出
-    ├── chosen_merge_00000.png      # 指定部分融合mask
-    ├── chosen_merge_116_ori.png
-    ├── merge_00000.png             # 融合mask
-    ├── merge_116_ori.png
-    ├── weighted_00000.png          # 叠加原图
-    ├── weighted_116_ori.png
-    ├── parsing_00000.png           # 分类结果，每个像素的值是[0, 18]
-    ├── parsing_116_ori.png
-    └── masks                       # 各部分mask
-        ├── 00000
-        │   ├── 00_background.png
-        │   ├── 01_skin.png
-        │   ├── 02_l_brow.png
-        │   ├── 03_r_brow.png
-        └── 116_ori
-            ├── 00_background.png
-            ├── 01_skin.png
-            ├── 02_l_brow.png
-            ├── 03_r_brow.png
 ```
 ```bash
-usage: face_parsing [-h] [--res_path RES_PATH] [--img_path IMG_PATH] [--ckpt CKPT] [--chosen_parts CHOSEN_PARTS [CHOSEN_PARTS ...]] [--reverse] [--color_style {face-parsing-style,CelebAMask-HQ-style}]
+usage: face_parsing [-h] [--ckpt CKPT] [--img_path IMG_PATH] [--save_root SAVE_ROOT] [--color_style {face-parsing-style,CelebAMask-HQ-style}] [--stride STRIDE] [--save_masks] [--masks_partition_by_name]
+                    [--save_parsing_anno] [--save_merge] [--save_weighted] [--chosen_parts CHOSEN_PARTS [CHOSEN_PARTS ...]] [--chosen_filename CHOSEN_FILENAME] [--chosen_reverse]
 
-options:
+optional arguments:
   -h, --help            show this help message and exit
-  --res_path RES_PATH   results path
-  --img_path IMG_PATH   data path
   --ckpt CKPT           checkpoint path
-  --chosen_parts CHOSEN_PARTS [CHOSEN_PARTS ...]
-                        chosen parts
-  --reverse             reverse the chosen parts
+  --img_path IMG_PATH   data path
+  --save_root SAVE_ROOT
+                        results path
   --color_style {face-parsing-style,CelebAMask-HQ-style}
                         color style
+  --stride STRIDE       stride
+  --save_masks          save masks
+  --masks_partition_by_name
+                        partition the masks image by name
+  --save_parsing_anno   save parsing annotation
+  --save_merge          save merge
+  --save_weighted       save weighted
+  --chosen_parts CHOSEN_PARTS [CHOSEN_PARTS ...]
+                        chosen parts
+  --chosen_filename CHOSEN_FILENAME
+                        image name
+  --chosen_reverse      reverse the chosen parts
 
 # 在face_parsing下
 $ face_parsing
 
 # 在face_parsing路径外
-$ face_parsing --ckpt ~/79999_iter.pth --res_path ~/test_res --img_path ~/test_img
+$ face_parsing --ckpt ~/79999_iter.pth
 ```
 
-| Index |   Name   | [face-parsing.PyTorch](https://github.com/zllrunning/face-parsing.PyTorch) Style RGB | [CelebAMask-HQ](https://github.com/switchablenorms/CelebAMask-HQ/blob/master/face_parsing/README.md) Style RGB |
-|-------|----------|------------------------|-------------------------|
-|   0   | background | [255, 0, 0]          | [0, 0, 0]               |
-|   1   | skin     | [255, 85, 0]          | [204, 0, 0]             |
-|   2   | l_brow   | [255, 170, 0]         | [0, 255, 255]           |
-|   3   | r_brow   | [255, 0, 85]          | [255, 204, 204]         |
-|   4   | l_eye    | [255, 0, 170]         | [51, 51, 255]           |
-|   5   | r_eye    | [0, 255, 0]           | [204, 0, 204]           |
-|   6   | eye_g    | [85, 255, 0]          | [204, 204, 0]           |
-|   7   | l_ear    | [170, 255, 0]         | [102, 51, 0]            |
-|   8   | r_ear    | [0, 255, 85]          | [255, 0, 0]             |
-|   9   | ear_r    | [0, 255, 170]         | [0, 204, 204]           |
-|   10  | nose     | [0, 0, 255]           | [76, 153, 0]            |
-|   11  | mouth    | [85, 0, 255]          | [102, 204, 0]           |
-|   12  | u_lip    | [170, 0, 255]         | [255, 255, 0]           |
-|   13  | l_lip    | [0, 85, 255]          | [0, 0, 153]             |
-|   14  | neck     | [0, 170, 255]         | [255, 153, 51]          |
-|   15  | neck_l   | [255, 255, 0]         | [0, 51, 0]              |
-|   16  | cloth    | [255, 255, 85]        | [0, 204, 0]             |
-|   17  | hair     | [255, 255, 170]       | [0, 0, 204]             |
-|   18  | hat      | [255, 0, 255]         | [255, 51, 153]          |
+# chosen_parts
 
-## face-parsing的模型效果
+```bash
+# neck和head前景（反选背景和cloth）
+$ subject='bala'
+$ face_parsing  --ckpt /home/lab/Documents/face-seg/pretrain/79999_iter.pth \
+    --img_path /media/lab/新加卷/DataSet/FlashAvatar/flash/dataset/$subject/imgs \
+    --save_root /media/lab/新加卷/DataSet/FlashAvatar/flash/dataset/$subject/parsing \
+	--chosen_parts 0 16 --chosen_filename neckhead --chosen_reverse
 
-![image-20240703160744779](https://cdn.jsdelivr.net/gh/sword4869/pic1@main/images/202407031607840.png)
+# mouth u_lip 和 lip
+$ face_parsing  --ckpt /home/lab/Documents/face-seg/pretrain/79999_iter.pth \
+    --img_path /media/lab/新加卷/DataSet/FlashAvatar/flash/dataset/$subject/imgs \
+    --save_root /media/lab/新加卷/DataSet/FlashAvatar/flash/dataset/$subject/parsing \
+	--chosen_parts 11 12 13 --chosen_filename mouth
+```
 
-![image-20240703160815874](https://cdn.jsdelivr.net/gh/sword4869/pic1@main/images/202407031608907.png)
-
-![image-20240703160828697](https://cdn.jsdelivr.net/gh/sword4869/pic1@main/images/202407031608737.png)
-
-# 参考资料
-
-> folk from https://github.com/zllrunning/face-parsing.PyTorch, https://github.com/dw-dengwei/face-seg
-
-1. [Code: BiSeNet](https://github.com/CoinCheung/BiSeNet)
-2. [Paper: BiSeNetV1](https://arxiv.org/abs/1808.00897)
-3. [Paper: BiSeNetV2](https://arxiv.org/abs/2004.02147)
+| Index |   Name   | [face-parsing.PyTorch](https://github.com/zllrunning/face-parsing.PyTorch) Style RGB | [CelebAMask-HQ](https://github.com/switchablenorms/CelebAMask-HQ/blob/master/face_parsing/README.md) Style RGB | 备注  |
+|-------|----------|------------------------|-------------------------|-----|
+|   0   | background | [255, 0, 0]          | [0, 0, 0]               |     |
+|   1   | skin     | [255, 85, 0]          | [204, 0, 0]             |     |
+|   2   | l_brow   | [255, 170, 0]         | [0, 255, 255]           |     |
+|   3   | r_brow   | [255, 0, 85]          | [255, 204, 204]         |     |
+|   4   | l_eye    | [255, 0, 170]         | [51, 51, 255]           |     |
+|   5   | r_eye    | [0, 255, 0]           | [204, 0, 204]           |     |
+|   6   | eye_g    | [85, 255, 0]          | [204, 204, 0]           |     |
+|   7   | l_ear    | [170, 255, 0]         | [102, 51, 0]            |     |
+|   8   | r_ear    | [0, 255, 85]          | [255, 0, 0]             |     |
+|   9   | ear_r    | [0, 255, 170]         | [0, 204, 204]           |     |
+|   10  | nose     | [0, 0, 255]           | [76, 153, 0]            |     |
+|   11  | mouth    | [85, 0, 255]          | [102, 204, 0]           | 嘴内  |
+|   12  | u_lip    | [170, 0, 255]         | [255, 255, 0]           | 上嘴唇 |
+|   13  | l_lip    | [0, 85, 255]          | [0, 0, 153]             | 下嘴唇 |
+|   14  | neck     | [0, 170, 255]         | [255, 153, 51]          |     |
+|   15  | neck_l   | [255, 255, 0]         | [0, 51, 0]              | 项链  |
+|   16  | cloth    | [255, 255, 85]        | [0, 204, 0]             |     |
+|   17  | hair     | [255, 255, 170]       | [0, 0, 204]             |     |
+|   18  | hat      | [255, 0, 255]         | [255, 51, 153]          |     |
--- src/face_parsing/segment.py
+++ src/face_parsing/segment.py
@@ -1,52 +1,51 @@
 #!/usr/bin/python
 # -*- encoding: utf-8 -*-
 
-from ast import arg
-from calendar import c
-from face_parsing.model import BiSeNet
-
-import torch
-
 import os
 import os.path as osp
-import numpy as np
-from PIL import Image
-import torchvision.transforms as transforms
+
 import cv2
+import numpy as np
+import torch
+import torchvision.transforms as transforms
+from PIL import Image
+from face_parsing.model import BiSeNet
 from tqdm import tqdm
 
 
 def vis_parsing_maps(
-    im,             
-    parsing_anno,   
-    stride,         
-    save_root,      
-    sample_name,    
-    color_style='face-parsing-style',
-    save_masks=True,
-    save_weighted=True,
-    save_merge=True,
-    save_parsing_anno=True,
+    im,
+    parsing_anno,
+    sample_name,
+    save_root=None,
+    color_style=None,
+    stride=None,
+    save_masks=None,
+    masks_partition_by_name=None,
+    save_parsing_anno=None,
+    save_merge=None,
+    save_weighted=None,
     chosen_parts=None,
-    reverse=False
+    chosen_filename=None,
+    chosen_reverse=None,
+    **kwargs
 ):
     '''将分割结果可视化
     Args:
         im: 输入图片 [512, 512, 3]
         parsing_anno: 每个像素上分割结果的类别, shape=(512, 512)
+        sample_name: 保存文件名
+
         stride: 缩放比例
         save_root: 保存路径
-        sample_name: 保存文件名
         color_style: 选择颜色
-            face-parsing-style 为 face-parsing.PyTorch 代码的颜色, 
+            face-parsing-style 为 face-parsing.PyTorch 代码的颜色,
             CelebAMask-HQ-style 为 CelebAMask-HQ的颜色 CelebAMask-HQ/face_parsing/Data_preprocessing/g_color.py
         save_masks: 是否保存每个部分的mask
+        partition_by_name： True则masks/00001/各个mask, False则masks/各个mask/00001
         save_weighted: 是否保存加权叠加的图片
         save_merge: 是否保存合并后的mask
     '''
-    os.makedirs(osp.join(save_root, 'masks', sample_name), exist_ok=True)
-    
-
     # 19 parts
     attr = {
         0: {
@@ -146,6 +145,12 @@ def vis_parsing_maps(
         },
     }
 
+    #
+    # if masks_partition_by_name:
+    #     os.makedirs(osp.join(save_root, 'masks', sample_name), exist_ok=True)
+    # else:
+    #     for i in range(19):
+    #         os.makedirs(osp.join(save_root, 'masks', attr[i]["name"]), exist_ok=True)
 
     vis_parsing_anno = parsing_anno.copy().astype(np.uint8)
     vis_parsing_anno = cv2.resize(vis_parsing_anno, None, fx=stride, fy=stride, interpolation=cv2.INTER_NEAREST)
@@ -153,77 +158,80 @@ def vis_parsing_maps(
     merge = np.zeros((vis_parsing_anno.shape[0], vis_parsing_anno.shape[1], 3))
     chosen_merge = np.zeros((vis_parsing_anno.shape[0], vis_parsing_anno.shape[1], 3))
 
+    if save_masks:
+        os.makedirs(osp.join(save_root, 'masks'), exist_ok=True)
+        if masks_partition_by_name:
+            for i in range(19):
+                os.makedirs(osp.join(save_root, 'masks', attr[i]["name"]), exist_ok=True)
 
     # 画出每个部分的mask，即 vis_parsing_anno_color
     for pi in np.unique(vis_parsing_anno):
         index = np.where(vis_parsing_anno == pi)
         # 每个部分：（彩色）vis_parsing_anno_color, （白色）mask
-        vis_parsing_anno_color = np.zeros((vis_parsing_anno.shape[0], vis_parsing_anno.shape[1], 3))
         mask = np.zeros((vis_parsing_anno.shape[0], vis_parsing_anno.shape[1], 3))
-        
+        if save_merge:
+            vis_parsing_anno_color = np.zeros((vis_parsing_anno.shape[0], vis_parsing_anno.shape[1], 3))
+
         # 赋值
-        vis_parsing_anno_color[index[0], index[1], :] = attr[pi][color_style]
         mask[index[0], index[1], :] = 255
-        merge += vis_parsing_anno_color
+        if save_merge:
+            vis_parsing_anno_color[index[0], index[1], :] = attr[pi][color_style]
+            merge += vis_parsing_anno_color
         if chosen_parts is not None and pi in chosen_parts:
             chosen_merge += mask
 
         if save_masks:
-            cv2.imwrite(
-                osp.join(save_root, 'masks', sample_name, f'{str(pi).zfill(2)}_{attr[pi]["name"]}.png'), 
-                mask
-            )
-    merge = merge.astype(np.uint8)
-    if merge.max() > 255:
-        raise ValueError('Color value out of range, please check the color value of each part.')
-    
-    # 加权叠加
-    if save_weighted:
-        im = np.array(im).astype(np.uint8)
-        vis_im = cv2.addWeighted(cv2.cvtColor(im, cv2.COLOR_RGB2BGR), 0.4, merge, 0.6, 0)
-        cv2.imwrite(osp.join(save_root, 'weighted_' + sample_name + '.png'), vis_im)
-
-    # 保存整体融合后的mask
-    if save_merge:
-        cv2.imwrite(osp.join(save_root, 'merge_' + sample_name + '.png') , merge)
-
-    # 保存指定融合后的mask
-    if chosen_parts is not None:
-        if reverse:
-            chosen_merge = 255 - chosen_merge
-            cv2.imwrite(osp.join(save_root, 'chosen_merge_' + sample_name + '.png') , chosen_merge)
+            mask_path = osp.join(save_root, 'masks', attr[pi]["name"], f'{sample_name}_{attr[pi]["name"]}.png')
+            if masks_partition_by_name:
+                mask_path = osp.join(save_root, 'masks', sample_name, f'{str(pi).zfill(2)}_{attr[pi]["name"]}.png')
+            cv2.imwrite(mask_path, mask)
 
     # 保存分割结果
     if save_parsing_anno:
-        cv2.imwrite(osp.join(save_root, 'parsing_' + sample_name + '.png'), vis_parsing_anno)
+        os.makedirs(osp.join(save_root, 'parsing'), exist_ok=True)
+        cv2.imwrite(osp.join(save_root, 'parsing', 'parsing_' + sample_name + '.png'), vis_parsing_anno)
+
+    # 保存整体融合后的mask
+    if save_merge:
+        os.makedirs(osp.join(save_root, 'merge'), exist_ok=True)
+        cv2.imwrite(osp.join(save_root, 'merge', 'merge_' + sample_name + '.png') , merge)
+
+    # 加权叠加
+    if save_weighted:
+        os.makedirs(osp.join(save_root, 'weighted'), exist_ok=True)
+        im = np.array(im).astype(np.uint8)
+        vis_im = cv2.addWeighted(cv2.cvtColor(im, cv2.COLOR_RGB2BGR), 0.4, merge, 0.6, 0)
+        cv2.imwrite(osp.join(save_root, 'weighted', 'weighted_' + sample_name + '.png'), vis_im)
+
+    # 保存指定融合后的mask
+    if chosen_parts is not None:
+        if chosen_reverse:
+            chosen_merge = 255 - chosen_merge
+        os.makedirs(osp.join(save_root, chosen_filename), exist_ok=True)
+        cv2.imwrite(osp.join(save_root, chosen_filename, f'{sample_name}_{chosen_filename}.png') , chosen_merge)
 
 
-def evaluate(img_path, res_path, ckpt, chosen_parts, reverse, color_style):
-    '''
-    Args:
-        img_path: 数据路径
-        res_path: 输出保存路径
-        ckpt: 模型路径
-        chosen_parts: 选择的部分
-    '''
 
-    if not os.path.exists(res_path):
-        os.makedirs(res_path)
-
+def evaluate(args):
     n_classes = 19
     net = BiSeNet(n_classes=n_classes)
     net.cuda()
-    net.load_state_dict(torch.load(ckpt))
+    net.load_state_dict(torch.load(args.ckpt))
     net.eval()
-    print('* loaded ckpt from {}'.format(ckpt))
+    print('* loaded ckpt from {}'.format(args.ckpt))
 
     to_tensor = transforms.Compose([
         transforms.ToTensor(),
         transforms.Normalize((0.485, 0.456, 0.406), (0.229, 0.224, 0.225)),
     ])
+
+    print('* evaluate on {}'.format(args.img_path))
+    print('* save results to {}'.format(args.save_root))
+    os.makedirs(args.save_root, exist_ok=True)
+
     with torch.no_grad():
-        for img_name in tqdm(os.listdir(img_path)):
-            image = Image.open(osp.join(img_path, img_name))
+        for img_name in tqdm(os.listdir(args.img_path)):
+            image = Image.open(osp.join(args.img_path, img_name))
             img = to_tensor(image)
             img = torch.unsqueeze(img, 0)
             img = img.cuda()
@@ -232,25 +240,31 @@ def evaluate(img_path, res_path, ckpt, chosen_parts, reverse, color_style):
             vis_parsing_maps(
                 image, 
                 parsing, 
-                stride=1, 
-                save_root=res_path,
-                sample_name=img_name[: -4],
-                chosen_parts=chosen_parts,
-                reverse=reverse,
-                color_style=color_style
+                img_name[: -4],
+                **vars(args)
             )
 
 def run_cli():
     import argparse
     parser = argparse.ArgumentParser()
-    parser.add_argument('--res_path', type=str, default='./test_res', help='results path')
-    parser.add_argument('--img_path', type=str, default='./test_img', help='data path')
     parser.add_argument('--ckpt', type=str, default='./pretrain/79999_iter.pth', help='checkpoint path')
-    parser.add_argument('--chosen_parts', type=int, nargs='+', default=None, help='chosen parts')
-    parser.add_argument('--reverse', action='store_true', help='reverse the chosen parts')
+    parser.add_argument('--img_path', type=str, default='./test_img', help='data path')
+    parser.add_argument('--save_root', type=str, default='./test_res', help='results path')
+
     parser.add_argument('--color_style', choices=['face-parsing-style', 'CelebAMask-HQ-style'], default='face-parsing-style', help='color style')
+    parser.add_argument('--stride', type=int, default=1, help='stride')
+
+    parser.add_argument('--save_masks', action='store_true', help='save masks')
+    parser.add_argument('--masks_partition_by_name', action='store_true', help='partition the masks image by name')
+
+    parser.add_argument('--save_parsing_anno', action='store_true', help='save parsing annotation')
+    parser.add_argument('--save_merge', action='store_true', help='save merge')
+    parser.add_argument('--save_weighted', action='store_true', help='save weighted')
+    parser.add_argument('--chosen_parts', type=int, nargs='+', default=None, help='chosen parts')
+    parser.add_argument('--chosen_filename', type=str, default='chose', help='image name')
+    parser.add_argument('--chosen_reverse', action='store_true', help='reverse the chosen parts')
     args = parser.parse_args()
-    evaluate(args.img_path, args.res_path, args.ckpt, args.chosen_parts, args.reverse, args.color_style)
+    evaluate(args)
 
 if __name__ == "__main__":
     run_cli()
